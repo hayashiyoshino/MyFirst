d69cc0a27b071d5ae1b05b2c1cf18d80cd43e80b
hayashiyoshino <twinklestaryoshino@gmail.com>
2023-01-29 10:59:25 +0900

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Yoshino Hayashi on 2023/01/29.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
