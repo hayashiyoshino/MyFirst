4a43c45e7f0023c05991f89074f931d702e53630
hayashiyoshino <twinklestaryoshino@gmail.com>
2023-01-29 16:12:48 +0900

    feature: ボタン追加

--- MyFirst/ContentView.swift
+++ MyFirst/ContentView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Hello, world!")
+                .font(.largeTitle)
+            Button("切り替えボタン") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .padding(.all)
+            .background(Color.blue)
+            .foregroundColor(.white)
+        }
     }
 }
 
